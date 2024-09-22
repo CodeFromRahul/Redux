fbca34fe7c205c63c3f86833336586a42d460bbc
CodeFromRahul <rahulvish194002@gmail.com>
2024-09-22 14:42:50 +0530

    Adding store in redux

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import AddTodo from './components/addTodo'
+import Todos from './components/Todos'
+function App() {
+
+
+  return (
+    <>
+     <h1>Add Todo</h1>
+     <AddTodo/>
+     <Todos/>
+    </>
+  )
+}
+
+export default App
